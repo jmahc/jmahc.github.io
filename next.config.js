c7d164c03cbfd21a3a1e8927bd154f58c97b2e88
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-14 13:38:07 -0500

    Added templates for `_app` and `_document`.  Added eslint configs.

--- next.config.js
+++ next.config.js
@@ -5,5 +5,11 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
+  // exportPathMap: function () {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/about": { page: "/about" },
+  //   }
+  // },
   assetPrefix: !debug ? '/jmahc.github.io/' : '',
 }
---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import App from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+
+import '../styles.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>My new cool app</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
+  }
+}
