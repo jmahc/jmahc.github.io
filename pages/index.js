f8f962ec24735554f06e4edc8d86a36a4933d7ee
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-14 13:38:07 -0500

    Added templates for `_app` and `_document`.  Added eslint configs.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+
+import Button from '../components/Button'
+
 export default function Home() {
   return (
     <div>
@@ -6,6 +9,8 @@ export default function Home() {
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
+      <Button />
+      <img src="/500.png" alt="500x500 image" className="main-photo" />
     </div>
   )
 }
