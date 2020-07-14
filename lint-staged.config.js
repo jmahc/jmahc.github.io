1d09fe0bf61bb9695811952212c4ee562a2dde01
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-14 13:39:48 -0500

    Updated lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   '*.md': ['prettier --write', 'git add'],
   'pages/**/*.js': [
-    'prettier --config ./.prettierrc.js --write "src/**/*.js"',
+    'prettier --config ./.prettierrc.js --write "pages/**/*.js"',
+    'git add',
+  ],
+  'components/**/*.js': [
+    'prettier --config ./.prettierrc.js --write "components/**/*.js"',
     'git add',
   ],
 }
