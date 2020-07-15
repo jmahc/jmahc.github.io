075f115b145995fe44f8fdb3b77106b418d98cb3
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-15 17:41:20 -0500

    Returning to what works with Vercel.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-const prod = process.env.NODE_ENV === 'production'
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
+  // Original
+  // assetPrefix: !debug ? '/jmahc.github.io/' : '',
   assetPrefix: '',
 }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,17 @@
 # Clean node_modules cache
+echo 'Clean node_modules cache'
 rm -rf node_modules/.cache
 # Clean recent output directory
+echo 'Clean recent output directory'
 rm -rf out
 # Build the project
+echo 'Build the project'
 ./node_modules/.bin/next build
 # Export the project to the output directory
+echo 'Export the project to the output directory'
 ./node_modules/.bin/next export
 # Use the `gh-pages` module to push the output directory
 # to the `master` branch, as this is a project that uses
 # my Github name
+# echo 'Pushing the output directory to the master branch'
 # ./node_modules/.bin/gh-pages -b master -d out
