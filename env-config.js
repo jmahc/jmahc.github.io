e6c365efdbd19514e0bfc59d5e210af90999890b
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-15 16:43:56 -0500

    Updated next/env configs and build scripts. Fingers crossed.

--- env-config.js
+++ env-config.js
@@ -1,8 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
-console.log('Env config: ', prod)
-
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/jmahc.github.io' : '',
-  // 'process.env.BACKEND_URL': prod ? '/' : '',
+  'process.env.BACKEND_URL': '',
 }
--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // exportPathMap: function () {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/about": { page: "/about" },
-  //   }
-  // },
-  assetPrefix: prod ? '/jmahc.github.io/' : '',
+  assetPrefix: '',
 }
