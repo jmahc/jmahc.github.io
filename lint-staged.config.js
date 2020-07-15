91965c887348cc20e43a16dcf1d99b14d6d48c94
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-15 09:01:28 -0500

    Updates

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.md': ['prettier --write', 'git add'],
-  'src/**/*.js': [
-    'prettier --config ./.prettierrc.js --write "src/**/*.js"',
-    'git add',
-  ],
-}
--- src/components/Title/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Title.module.css'
-
-export default function Title({ text }) {
-  return (
-    <div className={`mx-auto ${styles.flexBasis} p-4`}>
-      <h1 className="title">{text}</h1>
-    </div>
-  )
-}
